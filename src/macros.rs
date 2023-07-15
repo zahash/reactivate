589a8e7e1e5d6397af85603af59d4b922bea6a6d
zahash <zahash.z@gmail.com>
2023-07-15 18:53:04 +0530

    change merge_impl macro to impl_merge

--- src/macros.rs
+++ src/macros.rs
@@ -2,7 +2,7 @@ use crate::{Merge, Reactive};
 use paste::paste;
 use std::hash::Hash;
 
-macro_rules! merge_impl {
+macro_rules! impl_merge {
     ( $($i:literal,)* ) => { paste!{
         impl < $([<T $i>],)* > Merge for ( $( &Reactive<[<T $i>]>, )* )
         where
@@ -25,14 +25,14 @@ macro_rules! merge_impl {
         }};
 }
 
-merge_impl!(0, 1,);
-merge_impl!(0, 1, 2,);
-merge_impl!(0, 1, 2, 3,);
-merge_impl!(0, 1, 2, 3, 4,);
-merge_impl!(0, 1, 2, 3, 4, 5,);
-merge_impl!(0, 1, 2, 3, 4, 5, 6,);
-merge_impl!(0, 1, 2, 3, 4, 5, 6, 7,);
-merge_impl!(0, 1, 2, 3, 4, 5, 6, 7, 8,);
-merge_impl!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,);
-merge_impl!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,);
-merge_impl!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,);
+impl_merge!(0, 1,);
+impl_merge!(0, 1, 2,);
+impl_merge!(0, 1, 2, 3,);
+impl_merge!(0, 1, 2, 3, 4,);
+impl_merge!(0, 1, 2, 3, 4, 5,);
+impl_merge!(0, 1, 2, 3, 4, 5, 6,);
+impl_merge!(0, 1, 2, 3, 4, 5, 6, 7,);
+impl_merge!(0, 1, 2, 3, 4, 5, 6, 7, 8,);
+impl_merge!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,);
+impl_merge!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,);
+impl_merge!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,);
