d30f4bcf6bc5baa47daf2f877f2feac94d6c7a42
zahash <zahash.z@gmail.com>
2023-07-31 23:28:12 +0530

    impl Merge trait for tuples upto size 16

--- src/macros.rs
+++ src/macros.rs
@@ -40,3 +40,7 @@ impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8,);
 impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,);
 impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,);
 impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,);
+impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,);
+impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,);
+impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,);
+impl_merge_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,);
